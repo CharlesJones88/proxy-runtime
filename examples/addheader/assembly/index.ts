a8d82d32e04d0d1f16c0640f7ed950fec8ed1a41
Charles <ultramixerman@gmail.com>
2025-11-04 11:20:27 -0700

    Update libs and examples

--- examples/addheader/assembly/index.ts
+++ examples/addheader/assembly/index.ts
@@ -1,6 +1,11 @@
-
-export * from "@solo-io/proxy-runtime/assembly/proxy"; // this exports the required functions for the proxy to interact with us.
-import { RootContext, Context, registerRootContext, FilterHeadersStatusValues, stream_context } from "@solo-io/proxy-runtime/assembly";
+export * from "@charlesjones88/proxy-runtime/assembly/proxy"; // this exports the required functions for the proxy to interact with us.
+import {
+  RootContext,
+  Context,
+  registerRootContext,
+  FilterHeadersStatusValues,
+  stream_context,
+} from "@charlesjones88/proxy-runtime/assembly";
 
 class AddHeaderRoot extends RootContext {
   createContext(context_id: u32): Context {
@@ -17,10 +22,15 @@ class AddHeader extends Context {
     if (root_context.getConfiguration() == "") {
       stream_context.headers.response.add("hello", "world!");
     } else {
-      stream_context.headers.response.add("hello", root_context.getConfiguration());
+      stream_context.headers.response.add(
+        "hello",
+        root_context.getConfiguration()
+      );
     }
     return FilterHeadersStatusValues.Continue;
   }
 }
 
-registerRootContext((context_id: u32) => { return new AddHeaderRoot(context_id); }, "add_header");
+registerRootContext((context_id: u32) => {
+  return new AddHeaderRoot(context_id);
+}, "add_header");
